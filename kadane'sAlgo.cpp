6fcea716426d3a1bf735d5e38f6baa0a43e0efc9
Ashi Sharma <57591881+ashi-sharma@users.noreply.github.com>
2020-12-31 14:18:44 +0530

    Update kadane'sAlgo.cpp

--- kadane'sAlgo.cpp
+++ kadane'sAlgo.cpp
@@ -1,1 +1,11 @@
-
+int maxSubarraySum(int arr[], int n){
+    int maxsofar=0,maxendinghere=0;
+    for(int i=0;i<n;i++){
+        maxendinghere += arr[i];
+        if(maxendinghere>maxsofar)
+            maxsofar = maxendinghere;
+        else if(maxendinghere<0)
+            maxendinghere = 0;
+    }
+    return maxsofar;
+}
